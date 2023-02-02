f912d2bb2b5329339c35ad18ed3545cc098564e5
Kamila-Vieira <vkamila54@outlook.com>
2023-02-01 22:27:42 -0300

    iniciando a aplicação nest com sequelize

---
+++ lives/nest-hexagonal-arch/README.md
@@ -0,0 +1,20 @@
+## Aplicação Hexagonal com nestjs
+
+### Comandos úteis
+
+- Criar nova aplicação:
+````bash
+nest new <nome da aplicação>
+````
+
+- Criar novo recurso:
+````bash
+nest generate resource
+
+# será solicitado nome do recurso no plural. ex.: lists
+````
+
+- Instalar dependências para o Sequelize:
+````bash
+npm i sequelize sequelize-typescript sqlite3 @nestjs/sequelize
+````
---
+++ lives/nest-hexagonal-arch/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ListsModule } from './lists/lists.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { List } from './lists/entities/list.entity';
+
+@Module({
+  imports: [
+    ListsModule,
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      autoLoadModels: true,
+      host: ':memory:',
+      models: [List],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
