1bfe11373e44a80acdcc704c1073b5d1493286a9
Kamila-Vieira <vkamila54@outlook.com>
2023-02-10 01:56:40 -0300

    adicionando configuração inicial do nest (segunda aula)

---
+++ code-delivery/nest-api/.docker/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# run before chmod +x .docker/entrypoint.sh
+npm install
+
+npm run start:dev
---
+++ code-delivery/nest-api/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+//ES7 Descorators
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
