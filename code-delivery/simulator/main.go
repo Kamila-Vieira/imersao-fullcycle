02e54b2b030062d84073fc033c49f775d1bf8444
Kamila-Vieira <vkamila54@outlook.com>
2023-02-10 01:53:08 -0300

    adicionando a primeira aula de criação do simulador em Golang

--- code-delivery/simulator/main.go
+++ code-delivery/simulator/main.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 
-	"github.com/Kamila-Vieira/imersao-fullcycle/tree/main/code-delivery/simulator/application/route"
+	kafka2 "github.com/Kamila-Vieira/imersao-fullcycle/tree/main/code-delivery/simulator/application/kafka"
+	"github.com/Kamila-Vieira/imersao-fullcycle/tree/main/code-delivery/simulator/infra/kafka"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/joho/godotenv"
 )
 
-func main(){
-  route := route.Route{
-    ID: "1",
-    ClientId: "1",
-  }
-  
-  route.LoadPositions()
-  
-  stringJson, _ := route.ExportJsonPositions()
-  
-  fmt.Printf(stringJson[0])
+//{"clientId":"1","routeId":"1"}
+//{"clientId":"2","routeId":"2"}
+//{"clientId":"3","routeId":"3}
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
+
+func main() {
+	magChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(magChan)
+	go consumer.Consume()
+
+	for msg := range magChan {
+		fmt.Println(string(msg.Value))
+		go kafka2.Produce(msg)
+	}
 }
